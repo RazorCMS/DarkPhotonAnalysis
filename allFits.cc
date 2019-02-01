4cb5a709f94ce5912354e452a0501c465a26a7fb
Shu Fay Ung <shufay.ung@gmail.com>
2019-01-31 23:23:07 -0800

    fix s-only fit for JPsi plot, fix s+b fit for Psi_p plot

--- allFits.cc
+++ allFits.cc
@@ -1,6 +1,7 @@
 #include "RooRealVar.h"
 #include "RooDataSet.h"
 #include "RooDataHist.h"
+#include "RooAddPdf.h"
 #include "RooGaussian.h"
 #include "RooConstVar.h"
 #include "TCanvas.h"
@@ -32,25 +33,34 @@ int main(){
   RooWorkspace* w6 = (RooWorkspace*) f6->Get("LowMassFits") ;
 
   RooRealVar *m_mumu = w1->var("mass");
-/*
-  RooRealVar m_mumu("mass2", "m_{#mu#mu}", 1, 10., "GeV");
-  m_mumu.setRange("JPsi", 1.5, 3.5);
-  m_mumu.setRange("sig_plot", 2.8, 3.5);
-*/
 
+  RooAddPdf *sb_model1 = (RooAddPdf*) w1->pdf("sb_model");
+  RooAddPdf *sb_model2 = (RooAddPdf*) w2->pdf("sb_model");
+  RooAddPdf *sb_model3 = (RooAddPdf*) w3->pdf("sb_model");
+  RooAddPdf *sb_model4 = (RooAddPdf*) w4->pdf("sb_model");
+  RooAddPdf *sb_model5 = (RooAddPdf*) w5->pdf("sb_model");
+  RooAddPdf *sb_model6 = (RooAddPdf*) w6->pdf("sb_model");
+
+  /*
+   * Plot S+B
+   */
   TCanvas *csb = new TCanvas("csb", "csb", 900, 600);
   RooPlot *frameSB = new RooPlot("frameSB", "frameSB", *m_mumu, 1.5, 3.5, 800);
 //  RooPlot *frameSB = new RooPlot("frameSB", "frameSB", m_mumu, 1.5, 3.5, 800);
 
   w1->data("binned_mass")->plotOn(frameSB);
 
-  w1->pdf("sb_model")->plotOn(frameSB, RooFit::Name("single_exp"), RooFit::LineColor(kBlue), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
-  w2->pdf("sb_model")->plotOn(frameSB, RooFit::Name("double_exp"), RooFit::LineColor(kRed), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
-  w3->pdf("sb_model")->plotOn(frameSB, RooFit::Name("single_pow"), RooFit::LineColor(kGreen), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
-  w4->pdf("sb_model")->plotOn(frameSB, RooFit::Name("poly2"), RooFit::LineColor(kYellow), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
-  w5->pdf("sb_model")->plotOn(frameSB, RooFit::Name("poly3"), RooFit::LineColor(kMagenta), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
-  w6->pdf("sb_model")->plotOn(frameSB, RooFit::Name("poly4"), RooFit::LineColor(kCyan), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
-
+  sb_model1->plotOn(frameSB, RooFit::Name("single_exp"), RooFit::LineColor(kBlue), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
+  sb_model2->plotOn(frameSB, RooFit::Name("double_exp"), RooFit::LineColor(kRed), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
+  sb_model3->plotOn(frameSB, RooFit::Name("single_pow"), RooFit::LineColor(kGreen), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
+  sb_model4->plotOn(frameSB, RooFit::Name("poly2"), RooFit::LineColor(kYellow), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
+  sb_model5->plotOn(frameSB, RooFit::Name("poly3"), RooFit::LineColor(kMagenta), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
+  sb_model6->plotOn(frameSB, RooFit::Name("poly4"), RooFit::LineColor(kCyan), RooFit::Range("JPsi"), RooFit::NormRange("JPsi"));
+  
+  /*
+   * Plot S component only
+   */
+/*
   RooRealVar *alpha1_1=w1->var("DCB_NE_sig4_CB_alpha1");
   RooRealVar *alpha1_2=w2->var("DCB_NE_sig4_CB_alpha1");
   RooRealVar *alpha1_3=w3->var("DCB_NE_sig4_CB_alpha1");
@@ -99,16 +109,25 @@ int main(){
   RooDoubleCB *dcb4 = new RooDoubleCB("dcb4", "dcb4", *m_mumu,*mu_4, *sigma_4, *alpha1_4, *n1_4, *alpha2_4, *n2_4);
   RooDoubleCB *dcb5 = new RooDoubleCB("dcb5", "dcb5", *m_mumu,*mu_5, *sigma_5, *alpha1_5, *n1_5, *alpha2_5, *n2_5);
   RooDoubleCB *dcb6 = new RooDoubleCB("dcb6", "dcb6", *m_mumu,*mu_6, *sigma_6, *alpha1_6, *n1_6, *alpha2_6, *n2_6);
-  
-  RooPlot *frameS = new RooPlot("frameS", "frameS", *m_mumu, 1.5, 3.5, 800);
-  w1->data("binned_mass")->plotOn(frameS);
+   
   dcb1->plotOn(frameS, RooFit::Name("single_exp"), RooFit::LineColor(kRed), RooFit::Range("sig_plot"), RooFit::NormRange("sig_plot") );
   dcb2->plotOn(frameS, RooFit::Name("double_exp"), RooFit::LineColor(kBlue), RooFit::Range("sig_plot"), RooFit::NormRange("sig_plot") );
   dcb3->plotOn(frameS, RooFit::Name("single_pow"), RooFit::LineColor(kGreen), RooFit::Range("sig_plot"), RooFit::NormRange("sig_plot") );
   dcb4->plotOn(frameS, RooFit::Name("poly2"), RooFit::LineColor(kYellow), RooFit::Range("sig_plot"), RooFit::NormRange("sig_plot") );
   dcb5->plotOn(frameS, RooFit::Name("poly3"), RooFit::LineColor(kMagenta), RooFit::Range("sig_plot"), RooFit::NormRange("sig_plot") );
   dcb6->plotOn(frameS, RooFit::Name("poly4"), RooFit::LineColor(kCyan), RooFit::Range("sig_plot"), RooFit::NormRange("sig_plot") );
+*/
+
+  RooPlot *frameS = new RooPlot("frameS", "frameS", *m_mumu, 1.5, 3.5, 800);
+  w1->data("binned_mass")->plotOn(frameS);
   
+  sb_model1->plotOn(frameS, RooFit::Name("single_exp"), RooFit::LineColor(kBlue), RooFit::Components(sb_model1->pdfList()[0]));
+  sb_model2->plotOn(frameS, RooFit::Name("double_exp"), RooFit::LineColor(kRed), RooFit::Components(sb_model2->pdfList()[0]));
+  sb_model3->plotOn(frameS, RooFit::Name("single_pow"), RooFit::LineColor(kGreen), RooFit::Components(sb_model3->pdfList()[0]));
+  sb_model4->plotOn(frameS, RooFit::Name("poly2"), RooFit::LineColor(kYellow), RooFit::Components(sb_model4->pdfList()[0]));
+  sb_model5->plotOn(frameS, RooFit::Name("poly3"), RooFit::LineColor(kMagenta), RooFit::Components(sb_model5->pdfList()[0]));
+  sb_model6->plotOn(frameS, RooFit::Name("poly4"), RooFit::LineColor(kCyan), RooFit::Components(sb_model6->pdfList()[0]));
+
   csb->cd();
   frameSB->Draw();
   frameSB->SetTitle("");
